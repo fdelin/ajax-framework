9abe0d0ec6951b36a9ca09c4207c4d59738cc9e4
Fredrik Delin <fredrik@delin.uk>
2015-05-23 11:17:21 +0100

    ensures Internet Explorer uses the latest engine - read more here https://www.modern.ie/en-us/performance/how-to-use-x-ua-compatible

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
   <title>ajax page loading example</title>
   <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
 </head>
 
